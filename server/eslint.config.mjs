a999cf66107ddbc5aa27ad763b7eb1ddbe285f18
Szombati Olivér <o.szombati@gmail.com>
2025-03-28 09:04:09 +0100

    Removed prettier

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -10,26 +10,26 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
+  // eslintPluginPrettierRecommended,
+  // {
+  //   languageOptions: {
+  //     globals: {
+  //       ...globals.node,
+  //       ...globals.jest,
+  //     },
+  //     ecmaVersion: 5,
+  //     sourceType: 'module',
+  //     parserOptions: {
+  //       projectService: true,
+  //       tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  // },
+  // {
+  //   rules: {
+  //     '@typescript-eslint/no-explicit-any': 'off',
+  //     '@typescript-eslint/no-floating-promises': 'warn',
+  //     '@typescript-eslint/no-unsafe-argument': 'warn'
+  //   },
+  // },
 );
