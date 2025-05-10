8d164999c7de480350e60e84d8f3a592c1eae337
Szombati Olivér <o.szombati@gmail.com>
2025-05-10 15:00:16 +0200

    mod@client/areabox image placeholder added

--- client/src/app/music/areaBox.tsx
+++ client/src/app/music/areaBox.tsx
@@ -1,21 +1,64 @@
 import { Box, Paper, Typography } from "@mui/material";
 import { Area } from "@shared/area";
+import { Music } from "@shared/music";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface AreaBoxParameters{
     area: Area
 }
 export default function AreaBox({area}: AreaBoxParameters){
+    //TODO
+    // const [musics, setMusics] = useState<Music[]>([]);
+    // useEffect(() => {
+    //     async function fetchMusics() {
+    //         const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/music`);
+    //         if (response.status === 200) {
+    //             console.log("res", response.data);
+    //             console.log(area.id)
+    //             setMusics(response.data);
+    //         } else {
+    //             console.error("Failed to fetch music data");
+    //         }
+    //     }
+
+    //     fetchMusics();
+    // }, []);
+
     return (
         <>
         <Box sx={{position: "relative"}}>
-            <Paper sx={{padding: "1rem"}} elevation={3}>
-                <div style={{margin: ".5rem", marginLeft: "auto", marginRight: "auto", display: "block", position: "relative", width: "320px", height: "256px", backgroundColor: "red"}} className="image"></div>
+            <Paper sx={{padding: "1rem", overflow: "hidden"}} elevation={3}>
+                <div 
+                    style={{
+                        margin: ".5rem", 
+                        marginLeft: "auto", 
+                        marginRight: "auto", 
+                        display: "block", 
+                        position: "relative", 
+                        width: "100%", 
+                        maxWidth: "320px", 
+                        aspectRatio: "5 / 4", 
+                        // backgroundColor: "red"
+                    }} 
+                    className="image">
+                        <Image 
+                            src="https://placehold.co/600x400/EEE/31343C.png" 
+                            alt="Placeholder" 
+                            layout="fill" 
+                            objectFit="cover" 
+                        />
+                </div>
                 <Typography variant="h6">
                     <Link href={`/music/area/${area.id}`}>
                         {area.name}
                     </Link>
                 </Typography>
+                {/* <Typography variant="subtitle1" style={{fontStyle: "italic"}}>
+                    {musics.filter(m => m.area.id === area.id).length} dal
+                </Typography> */}
             </Paper>
         </Box>
         </>
